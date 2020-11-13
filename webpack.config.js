ac71a79f2c408191ec445305ca0bea0fb6d44d7f
Lotinex <loard969@gmail.com>
2020-11-13 21:09:01 +0900

    Setting

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const Path = require('path');
+
+module.exports = {
+    entry: "./src/Main.ts",
+    output: {
+        filename: "main.js",
+        path: Path.resolve(__dirname, 'dist/js')
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'source-map-loader',
+                enforce: 'pre' 
+            },
+            {
+                test: /\.ts$/,
+                enforce: "pre",
+                use: [
+                  {
+                    loader: "eslint-loader",
+                    options: {
+                      emitError: true,
+                      fix: true
+                    }
+                  }
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: [
+                  {
+                    loader: "ts-loader",
+                    /*
+                    options: {
+                      transpileOnly: true
+                    }*/
+                  }
+                ]
+            }
+        ]
+    },
+    devtool: 'source-map',
+}
